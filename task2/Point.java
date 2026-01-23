3992c76a0508c1b2eb18ec32bf9097b8514f7900
urunovulugbek <ulugbekutkurovich06@gmail.com>
2026-01-23 14:52:24 +0500

    Create Point.java

---
+++ task2/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+    private double x, y, z;
+
+    public Point() {}
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static void main(String[] args) {
+        Point ob1 = new Point(4.5, 4, 5.6);
+    }
+}
